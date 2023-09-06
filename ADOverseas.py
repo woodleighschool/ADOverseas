805bbc741257e362e6cde32b914c4f4a6c64f42e
Liam Matthews <liam@ethernetisoverrated.com>
2023-09-07 09:51:22 +1000

    Added better handling for dates in the past

--- ADOverseas.py
+++ ADOverseas.py
@@ -99,11 +99,15 @@ def schedule_user():
 
 
 def schedule(username, start_date, end_date):
-	app_log.debug(f"Adding data for job {username}_away to schedules database in case of system shutdown")
-	row_id = db.add_record(username, start_date, "leaving")
-	app_log.debug(f"Scheduling job {username}_away")
-	scheduler.add_job(ad.edit_ad_user, id=f"{username}_away", trigger='date', run_date=start_date, timezone=timezone.utc, args=[
-		username, 'away', row_id], replace_existing=True)
+	if start_date <= datetime.utcnow():
+		app_log.debug(f"Start date is in the past, adding {username} to group now instead")
+		scheduler.add_job(ad.edit_ad_user, id=f"{username}_away", args=[username, 'away', row_id], replace_existing=True)
+	else:
+		app_log.debug(f"Adding data for job {username}_away to schedules database in case of system shutdown")
+		row_id = db.add_record(username, start_date, "leaving")
+		app_log.debug(f"Scheduling job {username}_away")
+		scheduler.add_job(ad.edit_ad_user, id=f"{username}_away", trigger='date', run_date=start_date, timezone=timezone.utc, args=[
+			username, 'away', row_id], replace_existing=True)
 		
 	app_log.debug(f"Adding data for job {username}_home to schedules database in case of system shutdown")
 	row_id = db.add_record(username, end_date, "returning")
